541a80d21eb70c1ec199fcb58a71b6bd794c639f
vinicius5654 <vinicius_piai@estudante.sc.senai.br>
2016-09-21 20:09:08 +0000

    Dados pessoais

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'home#index'
-  
+  get 'home/dados_pessoais'
   get 'auth/:provider/callback', to: 'sessions#create'
   
   delete 'sign_out', to: 'sessions#destroy', as: 'sign_out'
